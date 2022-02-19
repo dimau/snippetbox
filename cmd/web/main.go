5c66f2c6f370453947e194ff91c667cfec4a742c
Dmitrii Ushakov <dimau777@gmail.com>
2022-02-20 00:50:28 +0700

    Config addr from CLI flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Используем стандартный пакет "flag"
+	// Объявляем параметр "addr"
+	// В первом аргументе - название ключа командной строки, чье значение парсим
+	// Во втором аргументе - значение по-умолчанию (если ключ не указан)
+	// В третьем аргументе - короткое текстовое объяснение, за что флаг отвечает
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Непосредственно выполняем парсинг всех флагов в соответствующие переменные
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -14,7 +24,11 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+
+	// Значение, возвращаемое функцией flag.String(), является указателем на
+	//    текстовое значение флага, а не самим значением.
+	// Поэтому, чтобы получить само текстовое значение, нам нужно добавить перед указателем символ *
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
