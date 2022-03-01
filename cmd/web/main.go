cbab2996281d0e8b86cb5c903e2e77b5eb8fe02c
Dmitrii Ushakov <dimau777@gmail.com>
2022-03-01 12:58:34 +0800

    Using HTTPS instead of HTTP

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"github.com/Dimau/snippetbox/pkg/models/mysql"
@@ -52,6 +53,7 @@ func main() {
 	// sessions always expires after 12 hours.
 	session := sessions.New([]byte(*secret))
 	session.Lifetime = 12 * time.Hour
+	session.Secure = true // Set the Secure flag on our session cookies
 
 	// Инициализируем инстанс структуры application, который будет содержать все зависимости для handler-ов HTTP запросов
 	app := &application{
@@ -62,16 +64,33 @@ func main() {
 		templateCache: templateCache,
 	}
 
+	// Initialize a tls.Config struct to hold the non-default TLS settings we want the server to use
+	//
+	// The tls.Config.PreferServerCipherSuites field controls whether the HTTPS connection should use
+	// Go’s favored cipher suites or the user’s favored cipher suites.
+	// By setting this to true — Go’s favored cipher suites are given preference
+	// and we help increase the likelihood that a strong cipher suite which also supports forward secrecy is used
+	//
+	// The tls.Config.CurvePreferences field lets us specify which elliptic curves should be given preference
+	// during the TLS handshake. Go supports a few elliptic curves, but as of Go 1.11 only
+	// tls.CurveP256 and tls.X25519 have assembly implementations. The others are very CPU intensive,
+	// so omitting them helps ensure that our server will remain performant under heavy loads.
+	tlsConfig := &tls.Config{
+		PreferServerCipherSuites: true,
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	// Инициализация сервера и роутера на базе пакета net/http
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
 		Handler: app.routes(),
+		TLSConfig: tlsConfig,
 	}
 
 	// Запуск сервера на базе пакета net/http
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
